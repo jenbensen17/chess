510458d8ed9fda61f3d18d315436b46372333f9a
jenbensen17 <jenbensen17@gmail.com>
2024-10-03 13:30:28 -0600

    Fix nextTurn function Pass off FullGameTest

--- shared/src/main/java/chess/GameState.java
+++ shared/src/main/java/chess/GameState.java
@@ -4,27 +4,41 @@ public class GameState {
     private ChessGame.TeamColor turn;
     private ChessBoard board;
     private final ChessRuleBook rules;
+
     public GameState() {
         turn = ChessGame.TeamColor.WHITE;
         board = new ChessBoard();
         board.resetBoard();
         rules = new ChessRuleBook();
     }
+
     public ChessGame.TeamColor turn() {
         return turn;
     }
+
     public void nextTurn() {
-        turn = turn == ChessGame.TeamColor.WHITE ? ChessGame.TeamColor.BLACK : turn;
+        switch (turn) {
+            case WHITE:
+                turn = ChessGame.TeamColor.BLACK;
+                break;
+            case BLACK:
+                turn = ChessGame.TeamColor.WHITE;
+                break;
+        }
     }
-    public ChessBoard board(){
+
+    public ChessBoard board() {
         return board;
     }
+
     public ChessRuleBook rules() {
         return rules;
     }
+
     public void setBoard(ChessBoard board) {
         this.board = board;
     }
+
     public void setTeamTurn(ChessGame.TeamColor team) {
         turn = team;
     }
