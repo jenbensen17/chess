60fae21ba6f3ecd753812b3e2dfbb3ff4cd98c64
jenbensen17 <jenbensen17@gmail.com>
2024-10-17 15:40:27 -0600

    Add skeleton for GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,33 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.AuthData;
+import model.GameData;
+
+import java.util.HashSet;
+
 public class GameService {
 
+    GameDAO gameDAO;
+    AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public HashSet<GameData> listGames(AuthData authData) throws DataAccessException {
+        return null;
+    }
+
+    public int createGame(GameData gameData, AuthData authData) throws DataAccessException {
+        return 0;
+    }
+
+    public void joinGame(GameData gameData, AuthData authData, ChessGame.TeamColor playerColor) throws DataAccessException {
+
+    }
 }
