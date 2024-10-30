d372ae539ea0a08e131ca18453cbdcefb747508f
jenbensen17 <jenbensen17@gmail.com>
2024-10-30 12:55:04 -0600

    Fix update game player color handling

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -96,18 +96,30 @@ public class SQLGameDAO implements GameDAO {
     @Override
     public void updateGame(int gameID, ChessGame.TeamColor playerColor, AuthData authData) throws DataAccessException {
         try(var conn = DatabaseManager.getConnection()) {
-            String colorToUpdate;
-            if(playerColor.equals(ChessGame.TeamColor.BLACK)) {
-                colorToUpdate = "black_username=?";
-            } else {
-                colorToUpdate = "white_username=?";
-            }
-            try(var preparedStatement = conn.prepareStatement("UPDATE gameTable SET "+colorToUpdate+" WHERE game_id = ?")) {
-                preparedStatement.setString(1,authData.getUsername());
-                preparedStatement.setInt(2, gameID);
-                int status = preparedStatement.executeUpdate();
-                if(status == 0) {
-                    throw new DataAccessException("Unable to update game");
+            try(var getColorStatement = conn.prepareStatement("SELECT white_username, black_username FROM gameTable WHERE game_id = ?")) {
+                getColorStatement.setInt(1, gameID);
+                try (var results = getColorStatement.executeQuery()) {
+                    results.next();
+                    var whiteUsername = results.getString("white_username");
+                    var blackUsername = results.getString("black_username");
+                    String colorToUpdate;
+                    if(playerColor == null) {
+                        throw new DataAccessException("Invalid player color");
+                    } else if(playerColor.equals(ChessGame.TeamColor.BLACK) && blackUsername==null ) {
+                        colorToUpdate = "black_username=?";
+                    } else if(playerColor.equals(ChessGame.TeamColor.WHITE) && whiteUsername==null ) {
+                        colorToUpdate = "white_username=?";
+                    } else {
+                        throw new DataAccessException("Error: Already Taken");
+                    }
+                    try(var preparedStatement = conn.prepareStatement("UPDATE gameTable SET "+colorToUpdate+" WHERE game_id = ?")) {
+                        preparedStatement.setString(1,authData.getUsername());
+                        preparedStatement.setInt(2, gameID);
+                        int status = preparedStatement.executeUpdate();
+                        if(status == 0) {
+                            throw new DataAccessException("Unable to update game");
+                        }
+                    }
                 }
             }
         } catch (SQLException e) {
