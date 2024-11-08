458d83fb9c43dde84e0e34140e2372a25b44ca45
jenbensen17 <jenbensen17@gmail.com>
2024-11-08 15:11:21 -0700

    Add help and quit commands to prelogin

--- client/src/main/java/ui/PreLoginRepl.java
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -3,15 +3,53 @@ package ui;
 import client.ServerFacade;
 import client.State;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PreLoginRepl {
     private final ServerFacade server;
     private State state = State.SIGNEDOUT;
+
     public PreLoginRepl(ServerFacade server) {
         this.server = server;
     }
 
-
     public void run() {
-        System.out.println("Pre-login repl");
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while(!result.toLowerCase().equals("quit")) {
+            String line = scanner.nextLine();
+            try{
+                result = eval(line);
+                System.out.println(result);
+            } catch(Throwable e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+    public String eval(String input) {
+        try{
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch(cmd) {
+              case "help" -> help();
+              case "quit" -> "quit";
+              default -> help();
+            };
+        } catch(Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private String help() {
+        String menu = """
+                register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                login <USERNAME> <PASSWORD> - to play chess
+                quit - playing chess
+                help - with possible commands
+                """;
+        return menu;
     }
 }
